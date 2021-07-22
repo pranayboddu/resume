4b431623efc4a86f26d58fd9d7d724497f84449e
Pranay Boddu <pranay.boddu@freedomcentral.com>
2021-07-22 20:39:23 +0530

    Typo Fix

--- src/components/ResumeFooter.tsx
+++ src/components/ResumeFooter.tsx
@@ -5,7 +5,7 @@ export default function ResumeFooter(props: any) {
     return (
         <Container className="footer px-0 mt-4 mb-5 align-items-center d-flex flex-column">
             <Col md={10}>
-                Thanks for you time :).
+                Thanks for your time.
                 {/* Thanks for you time. Hope you got my latest Resume.<br/> If not, you can find at <a href={"https://stealth-grid.github.io/resume"}>https://stealth-grid.github.io/resume</a>. */}
             </Col>
         </Container>)
